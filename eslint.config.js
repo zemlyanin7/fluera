ba9b2bdaecfb330a32f4c982ac0ac40c1c8bf55f
Андрей Алферонок <zemlyanin7@gmail.com>
2026-05-15 08:57:14 +0200

    chore: ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require('eslint/config');
+const expoConfig = require('eslint-config-expo/flat');
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    ignores: ['node_modules/', '.expo/', 'ios/', 'android/', 'assets/', 'dist/'],
+    rules: {
+      'react/jsx-no-undef': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+]);
